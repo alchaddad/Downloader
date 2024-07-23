d217d6a0ba5997705ea0b1ac72052773704ebcd7
alchaddad <79915729+alchaddad@users.noreply.github.com>
2024-07-23 18:47:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const schedule = document.getElementById('schedule');
 
     fetch('https://api.football-data.org/v2/competitions/PL/matches', {
-        headers: { 'X-Auth-Token': 'YOUR_API_KEY' }
+        headers: { 'X-Auth-Token': '0b9b6dc344a94f93a57e53756de1e29f' }
     })
     .then(response => response.json())
     .then(data => {
